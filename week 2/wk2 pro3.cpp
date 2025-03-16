05d3477eef0af7f33addc09118483431f2fa9068
jyoti-bisht-7 <jyotibisht71320@gmail.com>
2025-03-16 20:21:13 +0530

    Update wk2 pro3.cpp

--- week 2/wk2 pro3.cpp
+++ week 2/wk2 pro3.cpp
@@ -1,40 +1,56 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int difference(vector<int>arr,int k){
-	int n=arr.size();
-	int c=0;
-	sort(arr.begin(),arr.end());
+
+int difference(vector<int>& arr, int k) {
+    int n = arr.size();
+    int c = 0;
+    sort(arr.begin(), arr.end());
+    
     int left = 0;
     int right = 1;
-    while(left<n && right <n){
-        if(arr[right]-arr[left] == k ){
-    		left++;
-        	right++;
-        	c++;
-        }
-      	else if(arr[right] - arr[left] < k){
+    
+    while (right < n) {
+        if (left == right) {
             right++;
+            continue;
         }
-        else{
+
+        int diff = arr[right] - arr[left];
+        
+        if (diff == k) {
+            c++;
+            left++;
+            right++;
+            // Skip duplicates
+            while (left < n && arr[left] == arr[left - 1]) 
+		    left++;
+            while (right < n && arr[right] == arr[right - 1]) 
+		    right++;
+        } 
+	else if (diff < k) {
+            right++;
+        } 
+	else {
             left++;
         }
     }
     return c;
 }
-int main(){
-    int t ;
-    cin>>t;
-    while(t--){
-        int  n;
-        cin>>n;
-        vector<int>arr(n);
-        for(int i = 0;i<n;i++){
-          cin>>arr[i];
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
         }
-        int k,c; 
-        cin>>k;
-        c=difference(arr,k);
-        cout<<c<<endl;
+        int k;
+        cin >> k;
+        int c = difference(arr, k);
+        cout << c << endl;
     }
-    return 0;
+    return 0;
 }
